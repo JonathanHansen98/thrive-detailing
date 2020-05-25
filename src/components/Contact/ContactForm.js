3f68291254ec7e79e6d8107c83af4fdcf8fab4c6
bryceacampbell <bryceacampbell7@gmail.com>
2020-05-25 13:55:17 -0700

    pushing form submit

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button, Col, Form, Row} from "react-bootstrap";
+import { Button, Col, Form, Row } from "react-bootstrap";
 
 const ContactForm = () => {
     return (
@@ -9,57 +9,61 @@ const ContactForm = () => {
                     <h1>Contact Us</h1>
                 </Col>
             </Row>
-            <Form>
-                <Row>
-                    <Col>
-                        <Form.Label>First Name</Form.Label>
-                        <Form.Control type="input" placeholder="John" />
-                    </Col>
-                    <Col>
-                        <Form.Label>Last Name</Form.Label>
-                        <Form.Control type="input" placeholder="Smith" />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Form.Label>Email Address</Form.Label>
-                        <Form.Control type="email" placeholder="johnsmith@somewhere.com" />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Form.Label>Phone Number</Form.Label>
-                        <Form.Control type="input" placeholder="123-456-7890" />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Form.Group controlId="subject">
-                            <Form.Label>Subject</Form.Label>
-                            <Form.Control as="select">
-                                <option>Quote</option>
-                                <option>Question</option>
-                                <option>Comment</option>
-                            </Form.Control>
-                        </Form.Group>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Form.Group controlId="message">
-                            <Form.Label>Message:</Form.Label>
-                            <Form.Control as="textarea" rows="5" />
-                        </Form.Group>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Button variant='teal' type='submit' size="md" block>
-                            Submit
+            <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                <input type="hidden" name="bot-field" />
+                <input type="hidden" name="form-name" value="contact" />
+                <Form>
+                    <Row>
+                        <Col>
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control type="i" placeholder="John" id="first_name"/>
+                        </Col>
+                        <Col>
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control type="input" placeholder="Smith" id="last_name"/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control type="email" placeholder="johnsmith@somewhere.com" id="email"/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Form.Label>Phone Number</Form.Label>
+                            <Form.Control type="input" placeholder="123-456-7890" id="phone"/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Form.Group controlId="subject">
+                                <Form.Label>Subject</Form.Label>
+                                <Form.Control as="select" id="subject">
+                                    <option>Quote</option>
+                                    <option>Question</option>
+                                    <option>Comment</option>
+                                </Form.Control>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Form.Group controlId="message">
+                                <Form.Label>Message:</Form.Label>
+                                <Form.Control as="textarea" rows="5" id="message"/>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Button variant='teal' type='submit' size="md" block>
+                                Submit
                         </Button>
-                    </Col>
-                </Row>
-            </Form>
+                        </Col>
+                    </Row>
+                </Form>
+            </form>
         </div>
     );
 };
